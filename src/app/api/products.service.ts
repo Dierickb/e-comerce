5466d8a5b6f81b0aca72df15739a6edc6a732fba
Dierick Brochero <dierickbro@gmail.com>
2024-03-30 18:41:24 -0500

    add products service

---
+++ src/app/api/products.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject, signal } from "@angular/core";
+import { environment } from "@envs/environment";
+import { Product } from "@shared/models/product.interface";
+import { tap } from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class ProductsService {
+    public products = signal<Product[]>([]);
+    private readonly _http = inject(HttpClient);
+    private readonly _endPoint = environment.apiURL;
+
+    constructor() {
+        this.getProducts();
+    }
+
+    public getProducts(): void {
+        this._http.get<Product[]>(`${this._endPoint}?sort=desc`)
+            .pipe(tap((data: Product[]) => this.products.set(data)))
+        .subscribe()
+    }
+
+    public getProductById(id: string) {
+        return this._http.get<Product>(`${this._endPoint}/${id}`)
+    }
+
+    
+
+}
