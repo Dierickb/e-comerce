deae7086392adb620d958654329bf9c3b7a55d0d
Dierick Brochero <dierickbro@gmail.com>
2024-03-31 11:12:33 -0500

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsService } from '@api/products.service';
+import { HeaderComponent } from '@layaout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  private readonly productSvc = inject(ProductsService);
-  products = this.productSvc.products;
+  // private readonly productSvc = inject(ProductsService);
+  // products = this.productSvc.products;
 }
