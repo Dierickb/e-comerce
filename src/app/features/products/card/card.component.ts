87f6aa547e9aaf87ad7b5b06af84cafb516f75fc
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2024-04-26 20:28:10 -0500

    ad car link item

--- src/app/features/products/card/card.component.ts
+++ src/app/features/products/card/card.component.ts
@@ -1,10 +1,12 @@
+import { CurrencyPipe, SlicePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Product } from '@shared/models/product.interface';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, SlicePipe, RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
